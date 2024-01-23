8c1e537a3ea842e438d1ce4e9d835e355f4cb650
Hasnain <mrajhasnain101@gmail.com>
2024-01-23 18:47:21 +0600

    improvements needs to be done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Destination from "./components/Destination";
+import Expolore from "./components/Expolore";
+import Navbar from "./components/Navbar";
+import PopularTour from "./components/PopularTour";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar/>
+      <div class="mx-10 gap-10">
+        <Expolore/>
+        <PopularTour/>
+        <Destination/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Expolore.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ExploreImg from "../assets/explore.png";
+
+const Expolore = () => {
+  return (
+    <div className="flex items-center justify-center p-12 border-4">
+      <div className="relative">
+        <img src={ExploreImg} alt="explore" />
+
+        <div className="flex items-center justify-center flex-col absolute left-48 top-48 gap-5 text-white">
+          <div className="flex items-center justify-center flex-col gap-3">
+            <h1 className="text-4xl font-bold">Explore Your Travel</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae, doloremque quasi? Culpa rerum inventore harum?
+            </p>
+          </div>
+          <div className="bg-blue-500 opacity-80 p-2 w-11/12 h-14 rounded-lg relative">
+            <div className="flex items-center justify-center gap-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <input className="rounded-lg p-1.5" type="text" placeholder="Where to go" />
+              <input className="rounded-lg p-1.5" type="date" placeholder="When" />
+              <div className="relative">
+                <select className="rounded-lg p-1.5 appearance-none bg-transparent border-none text-white">
+                  <option value="" disabled selected>
+                    Select
+                  </option>
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
+                  <option value="option3">Option 3</option>
+                </select>
+                <span
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white"
+                  role="img"
+                  aria-label="select-icon"
+                >
+                  📌
+                </span>
+              </div>
+
+              <button className="flex items-center rounded-lg p-1.5 bg-orange-500 w-28">
+                <span role="img" aria-label="search-icon" className="mr-2">
+                  🔍
+                </span>
+                Find Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Expolore;
